02d0a3fd323b230781f1b76b8ccc17805c82c218
chenyl <yuelong_chen@yahoo.com>
2017-07-31 11:09:10 +0800

    生成setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='txt2excel',
+    version='v0.0.0.1_20170730_alpha',
+    packages=[''],
+    url='',
+    license='',
+    author='chenyuelong',
+    author_email='yuelong_chen@yahoo.com',
+    description='Trans text files in a dir to a Excel(xls)'
+)
