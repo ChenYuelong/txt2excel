9ceec7dc009238ec197f8f6113ec8ec63fcc03c9
chenyl <yuelong_chen@yahoo.com>
2017-07-31 11:10:08 +0800

    args变成*args，可以输入多个目录或者文件

--- trans2excel.py
+++ trans2excel.py
@@ -1,68 +1,80 @@
 #! /usr/bin/env python3
 
-import os, sys, re
-import xlrd
+import os
 import xlwt
 import readtxt
 
 
 class trans2excel():
-    def __init__(self, path, output='./tmp', sep='\t', type='xls'):
-        self.path = path
-        self.out = output
-        self.sep = sep
-        self.type = type
+    '''
+    trans text files to a excel file(.xls)
+    __init__:
+    *args：路径
+    **kwargs:
+    output:输出prefix，默认：./tmp
+    sep:分隔符，默认：\t
+    type:输出类型，默认：xls（暂时只能输出这种）
 
-    def isfile(self, mydir):
+    '''
+    def __init__(self, *path, output='./tmp', sep='\t', type='xls'):
+        self._path = path
+        self._out = output
+        self._sep = sep
+        self._type = type
+        self._files = []
+
+    def _isfile(self, mydir):
         for lists in os.listdir(mydir):
             path = os.path.join(mydir, lists)
 
             if os.path.isdir(path):
-                self.isfile(path)
+                self._isfile(path)
             else:
-                self.files.append(path)
+                self._files.append(path)
 
-    def getfiles(self):
-        self.files = []
-        if os.path.isdir(self.path):
-            self.isfile(self.path)
-        else:
-            self.files.append(self.path)
-        return self.files
+    def _getfiles(self):
+        for path in self._path:
+            if os.path.isdir(path):
+                self._isfile(path)
+            else:
+                self._files.append(path)
+            return self._files
 
-    def extractlines(self, file):
-        rt = readtxt.readtxt(file, self.sep)
+    def _extractlines(self, file):
+        rt = readtxt.readtxt(file, self._sep)
         lines = rt.readfile()
         return lines
 
     def generateExcel(self):
-        if not os.path.exists('{}.{}'.format(self.out, self.type)):
+        self._getfiles()
+        if not os.path.exists('{}.{}'.format(self._out, self._type)):
             workbook = xlwt.Workbook(encoding='ascii')
-            for efile in self.files:
+            for efile in self._files:
                 filename = os.path.basename(efile)
                 filename = filename.replace('.txt', '')
                 worksheet = workbook.add_sheet(filename)
-                mylines = self.extractlines(efile)
+                mylines = self._extractlines(efile)
                 row = 0
                 for line in mylines:
 
                     line = line.strip()
-                    cells = line.split(self.sep)
+                    cells = line.split(self._sep)
                     for col in range(0, len(cells)):
                         worksheet.write(row, col, label=cells[col])
                     row += 1
 
-            workbook.save('{}.{}'.format(self.out, self.type))
+            workbook.save('{}.{}'.format(self._out, self._type))
         else:
             print('({}.{}) is existed, please check the File!!\n'
-                  .format(self.out, self.type))
+                  .format(self._out, self._type))
 
 
 if __name__ == '__main__':
-    test = trans2excel('/annoroad/data1/bioinfo/PROJECT/Commercial/Medical/'
-                       'Leukemia/data/Commercial/HB_234_20170318105823/result/'
-                       'HB15CK00215-1-32/Variant/SNP-INDEL_MT/FILTER')
-    files = test.getfiles()
-    test.generateExcel()
-    for i in files:
-        print('file:{}'.format(i))
+    pass
+    # test = trans2excel('/annoroad/data1/bioinfo/PROJECT/Commercial/Medical/'
+    #                    'Leukemia/data/Commercial/HB_234_20170318105823/result/'
+    #                    'HB15CK00215-1-32/Variant/SNP-INDEL_MT/FILTER')
+    # files = test.getfiles()
+    # test.generateExcel()
+    # for i in files:
+    #     print('file:{}'.format(i))
